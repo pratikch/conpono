ef78aa4ad7e61bc9d879cca6dfdb7a23729f1a0d
pratikch <pratik.chaudhari@veritas.com>
2020-03-31 20:06:01 -0700

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { StoreListComponent } from './store-list/store-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store-list/store-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'conpono-store-list',
+  templateUrl: './store-list.component.html',
+  styleUrls: ['./store-list.component.scss']
+})
+export class StoreListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
